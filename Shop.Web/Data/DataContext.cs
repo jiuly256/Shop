49f9629fd8e17ce160020e9231982ec3fbcd401c
Jiuly Alexandra Rojas <jiuly256@gmail.com>
2019-10-16 16:07:20 -0400

    add products controller

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data.Entities;
+
+
+namespace Shop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
