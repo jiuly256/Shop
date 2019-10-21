c53f97675f4758de8fb0eb60f7eac933d687e1a9
Jiuly Alexandra Rojas <jiuly256@gmail.com>
2019-10-21 15:52:33 -0400

    images and other repository

--- Shop.Web/Data/ProductRepository.cs
+++ Shop.Web/Data/ProductRepository.cs
@@ -1,13 +1,21 @@
 ﻿
 namespace Shop.Web.Data
 {
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
     using Shop.Web.Data.Entities;
 
     public class ProductRepository : GenericRepository<Product>, IProductRepository
-
     {
+        private readonly DataContext context;
         public ProductRepository(DataContext context) : base(context)
         {
+            this.context = context;
+        }
+
+        public IQueryable GetAllWithUsers()
+        {
+            return this.context.Products.Include(p => p.User).OrderBy(p => p.Name);
         }
     }
 }
---
+++ Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Web.Models
+{
+    using Data.Entities;
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+
+
+}
